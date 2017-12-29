0daa46f66dc9461af668a3cb2f7aa78b22b54d4c
David Coutu <david.coutu@coxautoinc.com>
2017-12-29 15:00:55 -0500

    Completed first part of day 9.

--- src/main/scala/day9.scala
+++ src/main/scala/day9.scala
@@ -1,34 +1,76 @@
 object Day9 {
-	def readInput: List[Char] = {
-		scala.io.Source.fromFile("input/day9.txt").mkString.toList
+	def readInput: String = {
+		scala.io.Source.fromFile("src/main/resources/day9.txt").mkString
 	}
 	
-	sealed trait StreamProcessor {
-		def process(input: List[Char]): (List[Char], StreamProcessor)
+	def input(data: String): List[StreamProcessor] = {
+		process(data.toList)
 	}
+	
+	def scoreInput(data: String): Int = {
+		val groups = input(data)
+		
+		def loop(groups: List[StreamProcessor], depth: Int): Int = {
+			groups match {
+				case List() => 0
+				case head :: tail => head match {
+					case Group(children) => {
+						depth + loop(children, depth+1) + loop(tail, depth)
+					}
+					case _ => loop(tail, depth)
+				}
+			}
+		}
+		
+		loop(groups, 1)
+	}
+	
+	def process(input: List[Char]): List[StreamProcessor] = {
+		def loop(input: List[Char], roots: List[StreamProcessor] = List()): List[StreamProcessor] = {
+			input match {
+				case List() => roots
+				case head :: tail => head match {
+					case '<' => {
+						val (remainingInput, processedGarbage) = Garbage().process(tail)
+						loop(remainingInput, processedGarbage :: roots)
+					}
+					case '{' => {
+						val (remainingInput, processedGroup) = Group().process(tail)
+						loop(remainingInput, processedGroup :: roots)
+					}
+					case '!' => {
+						val (remainingInput, processedIgnore) = Ignore().process(tail)
+						loop(remainingInput, roots)
+					}
+					case _ => loop(tail, roots)
+				}
+			}
+		}
+		loop(input, List())
+	}
+	
+	sealed trait StreamProcessor
 	case class Group(children: List[StreamProcessor] = List()) extends StreamProcessor {
-		def process(input: List[Char]): (List[Char], StreamProcessor) = {
+		def process(input: List[Char], children: List[StreamProcessor] = List()): (List[Char], StreamProcessor) = {
 			input match {
 				case List() => (List(), this)
 				case head :: tail => head match {
 					case '!' => {
-						val initialIgnore = Ignore()
-						val (remainingInput, processedIgnore) = initialIgnore.process(tail)
-						process(remainingInput)
-						//process(remainingInput, processedGarbage :: processed)
+						val (remainingInput, processedIgnore) = Ignore().process(tail)
+						process(remainingInput, children)
 					}
 					case '{' => {
-						val initialGroup = Group()
-						val (remainingInput, processedGroup) = initialGroup.process(tail)
-						process(remainingInput)
+						val (remainingInput, processedGroup) = Group().process(tail)
+						process(remainingInput, processedGroup :: children)
 					}
-
-					case '}' => (tail, Group())
+					case '}' => {
+						(tail, Group(children))
+					}
 					case '<' => {
-						val initialGarbage = Garbage()
-						val (remainingInput, processedGarbage) = initialGarbage.process(tail)
-						process(remainingInput)
+						val (remainingInput, processedGarbage) = Garbage().process(tail)
+						process(remainingInput, processedGarbage :: children)
 					}
+					case _ => process(tail, children)
 				}
 			}
 		}
@@ -38,13 +80,11 @@ object Day9 {
 			input match {
 				case List() => (List(), this)
 				case head :: tail => head match {
-					case '!' => {
-						val initialIgnore = Ignore()
-						val (remainingInput, processedIgnore) = initialIgnore.process(tail)
-						process(remainingInput)
-						//process(remainingInput, processedIgnore :: processed)
-					}
 					case '>' => (tail, this)
+					case '!' => {
+						val (remainingInput, processedIgnore) = Ignore().process(tail)
+						process(remainingInput)
+					}
 					case _ => process(tail)
 				}
 			}
@@ -60,4 +100,7 @@ object Day9 {
 		}
 	}
 	
+	def main(args: Array[String]): Unit = {
+		println("score = " + scoreInput(readInput))
+	}
 }
--- src/test/scala/Day9Spec.scala
+++ src/test/scala/Day9Spec.scala
@@ -2,13 +2,55 @@ import org.scalatest._
 
 class Day9Spec extends FlatSpec with Matchers {
 
-  "An empty Set" should "have size 0" in {
-    assert(Set.empty.size == 1)
+  "Garbage" should "handle single blocks of garbage" in {
+    assert(Day9.input("<>") == List(Day9.Garbage()))
+		assert(Day9.input("<random characters>") == List(Day9.Garbage()))
+		assert(Day9.input("<<<<>") == List(Day9.Garbage()))
+		assert(Day9.input("""<{o"i!a,<{i<a>""") == List(Day9.Garbage()))
   }
+	
+	"Ignore" should "avoid closing garbage too soon" in {
+		assert(Day9.input("<{!>}>") == List(Day9.Garbage()))
+		assert(Day9.input("<!!>") == List(Day9.Garbage()))
+		assert(Day9.input("<!!!>>") == List(Day9.Garbage()))
+	}
+	
+	"Group" should "keep track of nested groups" in {
+		assert(Day9.input("{}") == List(Day9.Group()))
+		
+		assert(Day9.input("{{{}}}") == List(Day9.Group(List(Day9.Group(List(Day9.Group()))))))
+		
+		assert(Day9.input("{{},{}}") == List(Day9.Group(List(Day9.Group(), Day9.Group()))))
+		
+		assert(Day9.input("{{{},{},{{}}}}") ==
+			List(Day9.Group(
+				List(Day9.Group(
+					List(Day9.Group(List(Day9.Group())), Day9.Group(), Day9.Group())
+				))
+			))
+		)
+		
+		assert(Day9.input("{<{},{},{{}}>}") == List(Day9.Group(List(Day9.Garbage()))))
 
-  it should "produce NoSuchElementException when head is invoked" in {
-    assertThrows[NoSuchElementException] {
-      Set.empty.head
-    }
-  }
+		assert(Day9.input("{<a>,<a>,<a>,<a>}") == List(Day9.Group(List(Day9.Garbage(), Day9.Garbage(), Day9.Garbage(), Day9.Garbage()))))
+		
+		assert(Day9.input("{{<a>},{<a>},{<a>},{<a>}}") == List(Day9.Group(List(Day9.Group(List(Day9.Garbage())),
+																																					Day9.Group(List(Day9.Garbage())),
+																																					Day9.Group(List(Day9.Garbage())),
+																																					Day9.Group(List(Day9.Garbage()))))))
+																																					
+		assert(Day9.input("{{<!>},{<!>},{<!>},{<a>}}") == List(Day9.Group(List(Day9.Group(List(Day9.Garbage()))))))
+	}
+	
+	"Ignore" should "avoid closing group too soon" in {
+		assert(Day9.input("{!}{}}") == List(Day9.Group(List(Day9.Group()))))
+	}
+	
+	"Group" should "keep track of score" in {
+		assert(Day9.scoreInput("{}") == 1)
+		assert(Day9.scoreInput("{{{}}}") == 6)
+		assert(Day9.scoreInput("{{},{}}") == 5)
+		assert(Day9.scoreInput("{{{},{},{{}}}}") == 16)
+		assert(Day9.scoreInput("{{<a!>},{<a!>},{<a!>},{<ab>}}") == 3)
+	}
 }
